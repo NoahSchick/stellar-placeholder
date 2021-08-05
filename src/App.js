dc653e13c6a40f52427b908583abbf2c14448676
Noah Schick <noahschick8@gmail.com>
2021-08-05 19:20:31 -0400

    Add landing page content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a href="https://twitter.com/usestellarapp">@usestellarapp</a>
+      <div className="container">
+        <h1>UseStellar</h1>
+        <h3>Coming soon</h3>
+      </div>
     </div>
   );
 }
